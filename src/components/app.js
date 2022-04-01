152b95b1a9971604d5442b9ff3c904d46271bb55
sparebeat0814 <cannonball4114@gmail.com>
2022-03-31 21:08:55 -0600

    added the blog modal, even added the base line blog form

--- src/components/app.js
+++ src/components/app.js
@@ -3,19 +3,20 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import axios from "axios";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FortAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash, faSignOutAlt, faEdit } from "@fortawesome/free-solid-svg-icons"
+import { faTrash, faSignOutAlt, faEdit, faSpinner } from "@fortawesome/free-solid-svg-icons"
 
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+import BlogDetail from "./pages/blog-detail";
 import PortfolioManager from "./pages/portfolio-manager"
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
 import NoMatch from "./pages/no-match";
 
-library.add(faTrash, faSignOutAlt, faEdit);
+library.add(faTrash, faSignOutAlt, faEdit, faSpinner);
 
 export default class App extends Component {
   constructor(props) {
@@ -112,6 +113,7 @@ export default class App extends Component {
                <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} /> 
+              <Route path="/b/:slug" component={BlogDetail} /> 
                {this.state.loggedInStatus === "LOGGED_IN" ? (
                 this.authorizedPages()
               ) : null} *
